00f7a75ab7c13b7601f29bc7603645f25faf5eb0
KES0102 <mr.geka1@rambler.ru>
2025-10-31 14:13:08 +0300

    add mode 1x1: script2_1x1.js

---
+++ public/script2_1x1.js
@@ -0,0 +1,35 @@
+// Скрипт для игры 1 x 1
+let userActivity = true; //red
+function startGame1x1() {
+  console.log("1x1 start");
+  table.addEventListener("click", click1x1);
+  resetGame();
+  table.removeEventListener("click", clickUser);
+}
+
+function click1x1(event) {
+  console.log("table click");
+  const button = event.target;
+  if (
+    button.tagName == "BUTTON" &&
+    button.closest("#table").tagName == "TABLE"
+  ) {
+    if (userActivity) {
+      button.classList.add("butClickUser");
+      button.innerText = "X";
+      button.disabled = true;
+      array[button.getAttribute("id")] = 1;
+      console.log(array);
+      if (checkWinerAll(1) == 1) return;
+    }
+    if (!userActivity) {
+      button.classList.add("butClickComp");
+      button.innerText = "O";
+      button.disabled = true;
+      array[button.getAttribute("id")] = 2;
+      console.log(array);
+      if (checkWinerAll(2) == 1) return;
+    }
+    userActivity = !userActivity;
+  }
+}
